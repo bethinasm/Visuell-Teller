92d215e0b55af845b3e0f63dd578c6725ccbe30c
Bethina S.M <bethina.s.moen@gmail.com>
2024-09-04 11:07:16 +0200

    Oppdatert med MVC

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
+
+// MODEL
 var number = 0;
 
-function increaseButton(){
-    //let element = document.getElementById('numberText');
-    //let value = element.innerHTML;
 
-    //++value;
+// VIEW
+
+updateView()
+function updateView() {
+    document.getElementById('app').innerHTML = /*HTML*/ `
+        <div class="box">
+            <h1 id="numberText"> ${number} </h1>
+            <button onclick="increaseButton()"> + </button>
+            <button onclick="decreaseButton()"> - </button>
+        </div>
+    `;
+}
+
+// CONTROLLER
+
+function increaseButton(){
     number++;
 
     console.log(number);
     document.getElementById('numberText').innerHTML = number;
+
+    updateView();
 }
 
 function decreaseButton(){
-    //let element = document.getElementById('numberText');
-    //let value = element.innerHTML;
-
-    //--value;
     number--;
 
     console.log(number);
     document.getElementById('numberText').innerHTML = number;
+
+    updateView();
 }
 
 
